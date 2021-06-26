3cfd5fb1bff1d1687e43ea1e30c8bc77165f1747
devhypergeo <fitraelbiamrullah@gmail.com>
2021-06-26 14:24:48 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,171 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vl-map @click="clickCoordinate = $event.coordinate" :load-tiles-while-animating="true" :load-tiles-while-interacting="true" :data-projection="epsg" style="height: 400px">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+
+      <vl-layer-tile>
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+
+      <vl-layer-vector>
+        <vl-source-vector :features.sync="features"></vl-source-vector>
+      </vl-layer-vector>
+
+       <vl-interaction-select :features.sync="selectedFeatures" v-if="drawType == null">
+        <template slot-scope="select">
+          <!-- select styles -->
+          <vl-style-box>
+            <vl-style-stroke color="#423e9e" :width="7"></vl-style-stroke>
+            <vl-style-fill :color="[254, 178, 76, 0.7]"></vl-style-fill>
+            <vl-style-circle :radius="5">
+              <vl-style-stroke color="#423e9e" :width="7"></vl-style-stroke>
+              <vl-style-fill :color="[254, 178, 76, 0.7]"></vl-style-fill>
+            </vl-style-circle>
+          </vl-style-box>
+          <vl-style-box :z-index="1">
+            <vl-style-stroke color="#d43f45" :width="2"></vl-style-stroke>
+            <vl-style-circle :radius="5">
+              <vl-style-stroke color="#d43f45" :width="2"></vl-style-stroke>
+            </vl-style-circle>
+          </vl-style-box>
+          <!--// select styles -->
+
+          <!-- selected feature popup -->
+          <vl-overlay class="feature-popup" v-for="feature in select.features" :key="feature.id" :id="feature.id"
+                      :position="pointOnSurface(feature.geometry)" :auto-pan="true" :auto-pan-animation="{ duration: 300 }">
+            <template >
+              <section class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    Feature ID {{ feature.id }}
+                  </p>
+                  <a class="card-header-icon" title="Close"
+                     @click="selectedFeatures = selectedFeatures.filter(f => f.id !== feature.id)">
+                    <b-icon icon="close"></b-icon>
+                  </a>
+                </header>
+                <div class="card-content">
+                  <div style="padding: 10px;" class="content">
+                    <p>
+                      Note : {{ feature.properties.note}}
+                    </p>
+                    <p>
+                      Latitude :  {{ feature.properties.lat}}
+                    </p>
+                    <p>
+                      Longitude :  {{ feature.properties.lon}}
+                    </p>
+                    <p>
+                      Utm Code :  {{ feature.properties.utm_code}}
+                    </p>
+                    <p>
+                      Utm SRID :  {{ feature.properties.utm_srid}}
+                    </p>
+                    <div class="row d-flex justify-content-center">
+                      <button
+          type="button"
+          class="btn col-4 btn-modal project-create"
+          @click="sendProject"
+        >
+          Update
+        </button>
+                        <button
+          type="button"
+          class="btn col-4 btn-modal project-close"
+          @click="hideModal"
+        >
+          Delete
+        </button>
+        
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </template>
+          </vl-overlay>
+          <!--// selected popup -->
+        </template>
+      </vl-interaction-select>
+    </vl-map>
+    <p v-if="loading">
+      Loading features, please wait...
+    </p>
+    <p v-if="features.length > 0">
+      Loaded features: {{ features.map(feature => feature.id) }}
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { findPointOnSurface } from 'vuelayers/lib/ol-ext'
+  import axios from 'axios'
+  export default {
+    data () {
+      return { 
+        zoom: 18,
+        center: [106.816666, 	-6.200000],
+        rotation: 0,
+        features: [],
+        loading: false,
+        epsg: 'EPSG:4326',
+        data_api: [],
+        clickCoordinate: undefined,
+        selectedFeatures: [],
+        drawType: undefined,
+      }
+    },
+    mounted () {
+      const username = 'admin'
+      const password = 'admin'
+      const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
+      this.loading = true
+      this.loadFeatures().then(features => {
+        this.features = features.map(Object.freeze)
+        this.loading = false
+      })
+      axios.get('https://backend.widesight.id/image_objects/?as_geojson=true', {
+  headers: {
+ 'Authorization': `Basic ${token}`
+},
+}).then(res => {
+  console.log((res.data.features[0]).geometry.coordinates)
+  this.center = (res.data.features[0]).geometry.coordinates
+  this.data_api = res.data.features
+  })
+    },
+    methods: {
+      // emulates external source
+      pointOnSurface: findPointOnSurface,
+      loadFeatures () {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            // generate GeoJSON random features
+            resolve(this.data_api)
+          }, 3000)
+        })
+      },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    },
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.project-close {
+  color: white;
+  background: #ea868f;
+  margin: 10px;
+}
+.project-close:hover {
+  background: #f1aeb5;
+  color: white;
+}
+.project-create {
+  color: white;
+  background: #8cc152;
+  margin: 10px;
+}
+.project-create:hover {
+  background: #a0D468;
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import VueLayers modules
+import VueLayers from 'vuelayers'
+// import VueLayers styles
+import 'vuelayers/lib/style.css'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.use(VueLayers, {
+  // global data projection, see https://vuelayers.github.io/#/quickstart?id=global-data-projection
+  // dataProjection: 'EPSG:4326',
+})
+
 Vue.config.productionTip = false
 
 new Vue({
